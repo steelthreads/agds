ce056d2ebf0d8b2fab729ce28d5b54e05fafffcc
sukhrajghuman <sukhrajghuman@live.com>
2021-09-24 15:26:47 +1000

    REMOVE emotion :(

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,8 @@
 import React from "react";
-// there is an open issue about this
-import { ThemeProvider } from "emotion-theming";
-import { theme } from "@ag.ds/theme";
 
 export const decorators = [
   (Story, context) => {
-    return (
-      <ThemeProvider theme={theme}>
-        <Story />
-      </ThemeProvider>
-    );
+    return <Story />;
   },
 ];
 export const parameters = {
--- components/button/src/Button.tsx
+++ components/button/src/Button.tsx
@@ -1,22 +1,9 @@
 import React from "react";
-import styled from "@emotion/styled";
 
 interface ButtonProps {
   dark?: boolean;
 }
 
-const StyledButton = styled.button<ButtonProps>`
-  background: red;
-  color: #fff;
-  border-radius: 4px;
-  cursor: pointer;
-  font-size: 1rem;
-  font-weight: 300;
-  padding: 9px 36px;
-`;
-
 export const Button = React.forwardRef(
-  ({ dark }: React.PropsWithChildren<ButtonProps>, ref) => (
-    <StyledButton dark={dark}>hello</StyledButton>
-  )
+  ({ dark }: React.PropsWithChildren<ButtonProps>, ref) => <button></button>
 );
