4d3a99afd16bbc44994d3fbe0ad3c059a17927be
Elise Chant <elise.chant@gmail.com>
2021-09-29 16:23:08 +1000

    fix lint

--- packages/header/src/header.stories.js
+++ packages/header/src/header.stories.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import {Header} from '.';
+import { Header } from '.';
 
 export default {
-  title: 'AgDS/Header'
+  title: 'AgDS/Header',
 };
 
 export const Basic = () => <Header />;
