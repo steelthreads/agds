9238c60685c849147f19c696874cedfb5ee20eb9
Elise Chant <elise.chant@gmail.com>
2021-10-20 11:18:51 +1100

    fix types

--- packages/core/src/default-link.tsx
+++ packages/core/src/default-link.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-export interface LinkProps {
+// eslint-disable-next-line no-unused-vars
+export type LinkFactoryType = (p: LinkProps) => React.ReactNode;
+
+export type LinkProps = {
   href: string;
   children: React.ReactNode;
   [key: string]: any;
-}
+};
 
-// eslint-disable-next-line react/display-name
 export const Link = ({ href, children, ...rest }: LinkProps) => {
   return (
     <a href={href} {...rest}>
@@ -14,3 +16,5 @@ export const Link = ({ href, children, ...rest }: LinkProps) => {
     </a>
   );
 };
+
+Link.displayName = 'Link';
