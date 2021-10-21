ec01bb00c761e6004ea3b441709f2ef3885cd4d6
Elise Chant <elise.chant@gmail.com>
2021-10-21 15:52:05 +1100

    add select

---
+++ packages/select-field/src/select.stories.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+import { Select, Option } from '.';
+
+export default {
+  title: 'AgDS/Select',
+};
+
+export const Basic = () => {
+  const [value, setValue] = useState('');
+  return (
+    <>
+      <Select id={'select1'} value={value} onChange={(e) => setValue(e.target.value)}>
+        <Option value={'one'}>One</Option>
+        <Option value={'two'}>Two</Option>
+        <Option value={'three'}>Three</Option>
+      </Select>
+      <p>Value: {value}</p>
+    </>
+  );
+};
