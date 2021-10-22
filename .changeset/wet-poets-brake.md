9f4685e1ddae011dc97c0f17913224a0396ffd69
Elise Chant <elise.chant@gmail.com>
2021-10-22 15:05:25 +1100

    remove type

--- .changeset/wet-poets-brake.md
+++
@@ -1,5 +0,0 @@
----
-'@ag.ds/main-nav': patch
----
-
-Export UserMenuProps type
--- packages/main-nav/src/index.tsx
+++ packages/main-nav/src/index.tsx
@@ -1,3 +1,2 @@
 export { MainNav, MainNavLink } from './main-nav';
 export { UserMenu } from './user-menu';
-export type { UserMenuProps } from './user-menu'; 
