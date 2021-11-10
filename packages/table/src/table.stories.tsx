03546dd6811f75ca655e463400c828316a49e86c
Elise Chant <elise.chant@gmail.com>
2021-11-11 09:39:27 +1100

    Add table (#47)
    * add table
    
    * add changeset

---
+++ packages/table/src/table.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default {
+  title: 'AgDS/Table',
+};
+
+export const Basic = () => {
+  return <p>Currently exports React components directly from https://gold.designsystemau.org/components/table/</p>;
+};
